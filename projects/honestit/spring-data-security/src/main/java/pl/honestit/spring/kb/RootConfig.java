e93b82b70334ed140dd49c9966fe0e9413dd811e
honestit <michal.kupisinski@gmail.com>
2020-11-14 12:11:06 +0100

    Zadanie C.3.2 - Dodane klas konfiguracyjnych

--- projects/honestit/spring-data-security/src/main/java/pl/honestit/spring/kb/RootConfig.java
+++ projects/honestit/spring-data-security/src/main/java/pl/honestit/spring/kb/RootConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJpaRepositories(basePackages = "pl.honestit.spring.kb.data.repository")
 @EnableTransactionManagement
+@EnableWebSecurity
 public class RootConfig {
 
 }
---
+++ projects/honestit/spring-data-security/src/main/java/pl/honestit/spring/kb/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package pl.honestit.spring.kb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
