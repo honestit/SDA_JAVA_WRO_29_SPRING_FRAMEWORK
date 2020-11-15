d346099fc16bc5fb97b832036dd6295fcf50b5bb
honestit <michal.kupisinski@gmail.com>
2020-11-15 12:06:16 +0100

    Zadanie D.1.2 :: happy path test

--- projects/honestit/spring-data-security/src/test/java/pl/honestit/spring/kb/data/validation/validators/KnowledgeSourceValidatorTest.java
+++ projects/honestit/spring-data-security/src/test/java/pl/honestit/spring/kb/data/validation/validators/KnowledgeSourceValidatorTest.java
@@ -1,21 +1,42 @@
 package pl.honestit.spring.kb.data.validation.validators;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import pl.honestit.spring.kb.data.model.KnowledgeSource;
+import pl.honestit.spring.kb.data.model.Skill;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Specification for KnowledgeSourceValidator class")
 class KnowledgeSourceValidatorTest {
 
+    KnowledgeSourceValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new KnowledgeSourceValidator();
+    }
+
     @Nested
     @DisplayName("Specification for validating knowledge source for save")
     class IsValidForSave {
 
         @Test
-        void checkTest() {
+        @DisplayName("- should pass for valid knowledge source")
+        void test1() {
+            KnowledgeSource source = new KnowledgeSource();
+            source.setName("Alibaba i 7 rozbójników");
+            source.setDescription("Ciekawa książka o Alibabie, który ma 7 rozbójników i jest niczym Senior w gronie Juniorów :D");
+            source.setUrl("http://alibaba.com");
+            source.setActive(false);
+            Skill someSkill = new Skill();
+            someSkill.setId(1L);
+            source.setConnectedSkills(Set.of(someSkill));
 
+            boolean result = validator.isValidForSave(source);
+
+            Assertions.assertTrue(result, "Valid knowledge source did not pass");
         }
 
     }
